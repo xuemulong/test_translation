bf132abab0f26fd1f21896d5a16ba759dba3418d
subenchang <subenchang@xiaomi.com>
2017-05-18 20:18:01 +0800

    场景自动化多语言翻译申请

--- submit.sh
+++ submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd /home/share/test_translation
+cd /home/work/test_translation
 git status
 git diff
 git add .
 git commit -m "场景自动化多语言翻译申请"
-git pull
+git pull origin master
 git push origin master
 
